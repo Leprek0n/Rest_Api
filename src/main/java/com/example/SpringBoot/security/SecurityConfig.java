e9cbb4e8b27cff63d17516ad40c7d65f4ecace6d
Daulet Lepessov <fhrhrhhrueuu@gmail.com>
2021-09-17 04:23:53 +0600

    Rest Api

--- src/main/java/com/example/SpringBoot/security/SecurityConfig.java
+++ src/main/java/com/example/SpringBoot/security/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private UserDetailsServiceImpl userDetailsService;
     private SuccessHandler loginSuccessHandler;
+
     @Autowired
     public SecurityConfig(UserDetailsServiceImpl userDetailsService,
                      SuccessHandler loginSuccessHandler) {
